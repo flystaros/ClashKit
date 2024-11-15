8395cf4e6ae3a7474cfd2af29516f10d0d6545b4
qianli <qianli@qianlideMacBook-Air.local>
2024-11-15 17:09:12 +0800

    1.0

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "ClashKit",
+  products: [
+    .library(name: "ClashKit", targets: ["ClashKit"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "ClashKit",
+      url: "https://github.com/flystaros/ClashKit/releases/download/1.0/ClashKit.xcframework.zip"
+    )
+  ]
+)
