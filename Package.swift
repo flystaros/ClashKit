4c15f21590270e526f66b7d1f7a4442b575b0e72
qianli <qianli@qianlideMacBook-Air.local>
2024-11-15 17:33:32 +0800

    rm

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ClashKit",
-            url: "https://github.com/flystaros/ClashKit/releases/download/1.0/ClashKit.xcframework.zip",
+            url: "https://github.com/flystaros/ClashKit/releases/download/1.2/ClashKit.xcframework.zip",
             checksum: "f47471bc40ad5bc8e0967b8c07be5874f3bab916afa1b6c9d3a5e7b0d44ffa79" // 这里需要填写正确的校验和
         )
     ]
